a8adf2d4ed52e83df1768ad254a83406090d93fb
Yu-Chen Hsu <every2ever@gmail.com>
2016-12-31 14:12:20 -0800

    #2 make no transition for hour/min/ hands when go back to start point (90deg)

--- 02_CSS_JS_Clock/js/main.js
+++ 02_CSS_JS_Clock/js/main.js
@@ -37,19 +37,35 @@
 
 	const view = {
 		init: function () {
+			this.stopDegree = 444;
+			this.restartDegree = 96;
 			this.hourHand = document.getElementsByClassName('hour-hand')[0];
 			this.minHand = document.getElementsByClassName('min-hand')[0];
 			this.secHand = document.getElementsByClassName('sec-hand')[0];
 			this.render();
 		},
 
+		handleNotrasition: function (hour, min, sec) {
+			if (sec === this.stopDegree) {
+				this.secHand.classList.add('notransition');
+				if (hour === this.stopDegree) 
+					this.hourHand.classList.add('notransition');
+				if (min === this.stopDegree) 
+					this.minHand.classList.add('notransition');
+				
+			} else if (sec === this.restartDegree) {
+				this.secHand.classList.remove('notransition');
+				if (hour === this.restartDegree) 
+					this.hourHand.classList.remove('notransition');
+				if (min === this.restartDegree) 
+					this.minHand.classList.remove('notransition');
+			}
+		},
+
 		render: function () {
 			let degrees = control.getDegrees();
-			if (degrees.sec === 444) {
-				this.secHand.classList.add('notransition');
-			} else if (degrees.sec === 96) {
-				this.secHand.classList.remove('notransition');
-			}
+			this.handleNotrasition(degrees.hour, degrees.min, degrees.sec);
+			
 			this.hourHand.style.transform = `rotate(${degrees.hour}deg)`;
 			this.minHand.style.transform = `rotate(${degrees.min}deg)`;
 			this.secHand.style.transform = `rotate(${degrees.sec}deg)`;
